f768845ce6fe3e78d999908cc63153fbf61def89
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2024-10-22 00:28:51 +0300

    Добавление функции загрузки данных конфигурации сайта.

--- api/index.php
+++ api/index.php
@@ -1,7 +1,12 @@
 <?php
+
+    include_once "./init.php";
+
     echo "<h3>Backend api-интерфейс портала</h3>";
     echo "<p>Для ввода запросов используются: <ul>
         <li>Параметры url-запроса;</li>
         <li>GET-параметры;</li>
     </ul></p>";
+    $data = loadConfigData("config.json");
+    var_dump($data);
 ?>
