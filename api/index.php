d3ff2ba8bcc4f14bad263c72ffd409e92b172641
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2024-10-23 14:32:39 +0300

    Добавление классов-моделей базы данных и пользователей.

--- api/index.php
+++ api/index.php
@@ -1,12 +1,13 @@
 <?php
 
-    include_once "./init.php";
+    include_once "./models/users.php";
 
     echo "<h3>Backend api-интерфейс портала</h3>";
     echo "<p>Для ввода запросов используются: <ul>
         <li>Параметры url-запроса;</li>
         <li>GET-параметры;</li>
     </ul></p>";
-    $data = loadConfigData("config.json");
-    var_dump($data);
+
+    $users = _Users::getUsersFromDB();
+    var_dump($users);
 ?>
--- api/init.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-    function loadConfigData($initfile){
-        $result = file_get_contents($initfile);
-        return json_decode($result);
-    }
-
-?>
